56915055a1ca19f0b90515e9488867358eb20274
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-06-19 18:33:03 +0200

    feat: made the table in the index page dark

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,32 +3,35 @@
 @section('content')
     <div class="container">
         <h2 class="my-3">Lista progetti</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Immagine</th>
-                    <th scope="col">Titolo</th>
-                    <th scope="col">Sito</th>
-                    <th scope="col">Data inizio</th>
-                    <th scope="col">Data fine</th>
-                    <th scope="col">Azioni</th>
+        <div class="container-table mb-3">
 
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($projectList as $project)
-                <tr>
-                    <td><img src="{{$project->image_url}}" alt=""></td>
-                    <th scope="row">{{$project->title}}</th>
-                    <td class="site"><a href="{{$project->url_site}}">{{$project->url_site}}</a></td>
-                    <td>{{$project->start_date}}</td> 
-                    <td>{{$project->finish_date}}</td>
-                    <td><button>Prova</button></td>
-                </tr>
-                    
-                @endforeach
-
-            </tbody>
-        </table>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Immagine</th>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Sito</th>
+                        <th scope="col">Data inizio</th>
+                        <th scope="col">Data fine</th>
+                        <th scope="col">Azioni</th>
+    
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($projectList as $project)
+                    <tr>
+                        <td><img src="{{$project->image_url}}" alt=""></td>
+                        <th scope="row">{{$project->title}}</th>
+                        <td class="site"><a href="{{$project->site_url}}">{{$project->site_url}}</a></td>
+                        <td>{{$project->start_date}}</td> 
+                        <td>{{$project->finish_date}}</td>
+                        <td><button>Prova</button></td>
+                    </tr>
+                        
+                    @endforeach
+    
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
