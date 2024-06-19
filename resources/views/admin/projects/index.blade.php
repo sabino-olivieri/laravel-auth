de975fb20533d7c6fdc86432a33879d032f503b2
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-06-19 16:54:46 +0200

    feat: model, view and controller created for projects table

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h2>Index</h2>
+@endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,10 +10,7 @@
 
   <title>{{ config('app.name', 'Laravel') }}</title>
 
-  <!-- Fontawesome 6 cdn -->
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
-    integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
-    crossorigin='anonymous' referrerpolicy='no-referrer' />
+
 
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -28,7 +25,7 @@
 
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
       <div class="row justify-content-between">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolPress</a>
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolFolio</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
           data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
           aria-label="Toggle navigation">
@@ -58,9 +55,15 @@
           <div class="position-sticky pt-3">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
+                <a class="nav-link text-white rounded-2 {{ Request::route()->getName() == 'admin.dashboard' ? 'bg-primary' : '' }}"
                   href="{{ route('admin.dashboard') }}">
                   <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white rounded-2 {{ Request::route()->getName() == 'admin.project.index' ? 'bg-primary' : ''}}"
+                  href="{{ route('admin.project.index') }}">
+                  <i class="fa-solid fa-table-list"></i> Lista Progetti
                 </a>
               </li>
             </ul>
