d749c45e2d9a03392a2842a1430077b81c0d900c
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-06-20 18:36:15 +0200

    feat: add toast notification for update and store, fix method update

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -52,7 +52,7 @@ class ProjectController extends Controller
         $newProject->fill($request->all());
         $newProject->slug = Str::slug($newProject->title);
         $newProject->save();
-        return redirect()->route('admin.project.show', ['project' => $newProject->slug]);
+        return redirect()->route('admin.project.show', ['project' => $newProject->slug])->with('message', 'Progetto '.$newProject->title.' salvato corrattamente');
     }
 
     /**
@@ -85,11 +85,11 @@ class ProjectController extends Controller
      */
     public function update(UpdateProjectRequest $request, Project $project)
     {
-        $newProject = new Project();
-        $newProject->fill($request->all());
-        $newProject->slug = Str::slug($newProject->title);
-        $newProject->save();
-        return redirect()->route('admin.project.show', ['project' => $newProject->slug]);
+
+        $project->fill($request->all());
+        $project->slug = Str::slug($project->title);
+        $project->save();
+        return redirect()->route('admin.project.show', ['project' => $project->slug])->with('message', 'Progetto '.$project->title.' modificato corrattamente');
     }
 
     /**
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,28 +4,43 @@
     <div class="container">
         <div class="row">
             <div class="col-12 my-3">
-                <h2>{{$project->title}}</h2>
+                <h2>{{ $project->title }}</h2>
             </div>
             <div class="col-12 col-md-6">
-                <img class="img-fluid" src="{{$project->image_url}}" alt="">
+                <img class="img-fluid" src="{{ $project->image_url }}" alt="">
             </div>
 
             <div class="col-12 col-md-6">
                 <ul class="list-unstyled">
                     <li class="mb-3">
-                        <strong>Data inizio progetto:</strong> {{$project->start_date}}
+                        <strong>Data inizio progetto:</strong> {{ $project->start_date }}
                     </li>
                     <li class="mb-3">
-                        <strong>Data fine progetto:</strong> {{$project->finish_date}}
+                        <strong>Data fine progetto:</strong> {{ $project->finish_date }}
                     </li>
                     <li class="text-truncate mb-3">
-                        <strong>Link progetto:</strong> <a href="{{$project->site_url}}">{{$project->site_url}}</a>
+                        <strong>Link progetto:</strong> <a href="{{ $project->site_url }}">{{ $project->site_url }}</a>
                     </li>
                     <li class="mb-3">
-                        <strong>Descrizione: </strong> {{$project->description}}
+                        <strong>Descrizione: </strong> {{ $project->description }}
                     </li>
                 </ul>
             </div>
         </div>
     </div>
+
+    @session('message')
+        <div class="ms_toast ms_hidden bg-success rounded-2 shadow">
+            {{ session('message') }} <i class="fa-regular fa-circle-check"></i>
+        </div>
+
+        <script>
+            const toastElem = document.querySelector('.ms_toast');
+            toastElem.classList.toggle('ms_hidden');
+
+            const time = setTimeout(() => {
+                toastElem.classList.toggle('ms_hidden');
+            }, 5000);
+        </script>
+    @endsession
 @endsection
